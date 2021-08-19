1520fbdf261d97844ff359c49c36fea06335ae2c
fritzmg <fmg@inspiredminds.at>
2021-08-19 20:36:10 +0100

    implement counts

--- src/EventListener/GetAllEventsListener.php
+++ src/EventListener/GetAllEventsListener.php
@@ -20,42 +20,91 @@ class GetAllEventsListener
 {
     public function onGetAllEvents(array $events, array $calendars, int $start, int $end, Module $module): array
     {
-        if ($module->jumpTo && null !== ($jumpTo = PageModel::findByPk($module->jumpTo))) {
-            foreach ($events as &$eventsPerDay) {
-                foreach ($eventsPerDay as &$eventsOnThatDay) {
-                    foreach ($eventsOnThatDay as &$event) {
-                        if (empty($event['source']) || 'default' === $event['source']) {
-                            $event['href'] = ampersand($jumpTo->getFrontendUrl((Config::get('useAutoItem') ? '/' : '/events/').($event['alias'] ?: $event['id'])));
-                        }
-                    }
-                }
-            }
-        }
-
-        if ((int) $module->skipFirst > 0) {
-            $processed = [];
-            $count = 0;
-
-            foreach ($events as $groupKey => &$groupEvents) {
-                foreach ($groupEvents as $dateKey => $dateEvents) {
-                    foreach ($dateEvents as $event) {
-                        // Skip any events outside the scope
-                        if ((int) $event['begin'] < $start || (int) $event['end'] > $end) {
-                            continue;
-                        }
-
-                        ++$count;
-
-                        if ($count > (int) $module->skipFirst) {
-                            $processed[$groupKey][$dateKey][] = $event;
-                        }
-                    }
-                }
-            }
-
-            $events = $processed;
-        }
+        $this->applySkipFirst($events, $start, $end, $module);
+        $this->overrideJumpTo($events, $module);
+        $this->addCounts($events, $module);
 
         return $events;
+    }
+
+    private function overrideJumpTo(array &$events, Module $module): void
+    {
+        if (empty($module->jumpTo)) {
+            return;
+        }
+
+        $jumpTo = PageModel::findByPk($module->jumpTo);
+
+        if (null === $jumpTo) {
+            return;
+        }
+
+        foreach ($events as &$eventsPerDay) {
+            foreach ($eventsPerDay as &$eventsOnThatDay) {
+                foreach ($eventsOnThatDay as &$event) {
+                    if (empty($event['source']) || 'default' === $event['source']) {
+                        $event['href'] = ampersand($jumpTo->getFrontendUrl((Config::get('useAutoItem') ? '/' : '/events/').($event['alias'] ?: $event['id'])));
+                    }
+                }
+            }
+        }
+    }
+
+    private function applySkipFirst(array &$events,  int $start, int $end, Module $module): void
+    {
+        if ((int) $module->skipFirst <= 0) {
+            return;
+        }
+
+        $processed = [];
+        $count = 0;
+
+        foreach ($events as $groupKey => $groupEvents) {
+            foreach ($groupEvents as $dateKey => $dateEvents) {
+                foreach ($dateEvents as $event) {
+                    // Skip any events outside the scope
+                    if ((int) $event['begin'] < $start || (int) $event['end'] > $end) {
+                        continue;
+                    }
+
+                    ++$count;
+
+                    if ($count > (int) $module->skipFirst) {
+                        $processed[$groupKey][$dateKey][] = $event;
+                    }
+                }
+            }
+        }
+
+        $events = $processed;
+    }
+
+    private function addCounts(array &$events, Module $module): void
+    {
+        $total = 0;
+
+        foreach ($events as $groupKey => &$groupEvents) {
+            foreach ($groupEvents as $dateKey => &$dateEvents) {
+                foreach ($dateEvents as &$event) {
+                    ++$total;
+                }
+            }
+        }
+
+        $count = 0;
+
+        foreach ($events as $groupKey => &$groupEvents) {
+            foreach ($groupEvents as $dateKey => &$dateEvents) {
+                foreach ($dateEvents as &$event) {
+                    ++$count;
+                    $countAscending = $count;
+                    $countDescending = $total - ($count - 1);
+                    $event['countNumber'] = $module->cal_order === 'descending' ? $countDescending : $countAscending;
+                    $event['countAscending'] = $countAscending;
+                    $event['countDescending'] = $countDescending;
+                    $event['countTotal'] = $total;
+                }
+            }
+        }
     }
 }
